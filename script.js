1e95a39d4c94faf4fc6da6eb80e736e2158790a9
l229926929 <3179808443@qq.com>
2025-03-30 19:32:35 +0800

    feat: 添加加法计算器功能

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ function processForm() {
   //在控制台中显示问候信息
   console.log(greeting);
 }
+
+function alert() {
+  let num1 = document.getElementById('num1').value;
+  let num2 = document.getElementById('num2').value;
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+
+  let sum=num1+num2;
+
+  document.getElementById('sum_number').innerText = sum;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    console.error("请输入有效的数字");
+  }
+}
